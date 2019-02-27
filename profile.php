87b3eba8780fba87f509e738160f99b9fcbbb5c9
Eakkk <eakarat.sa@u.th>
2019-02-28 00:29:13 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,25 +2,27 @@
 
 $access_token = 'j7iVbv/hCyo7SnTPRSbrjYnaRoCrdmgUOIjiO91utTjw3zXnmU+E/opAjIBW/hRoNwgLRE9Uw3w1BjU2NP8VXhjhtUohLxrJoWi2U26cCeQFnFhl1IJLPoC8TLo44wBwJdnVgK2NN//5JkvWisdnZgdB04t89/1O/w1cDnyilFU=';
 
+$channelSecret = '6c82b4408e6a4b534ed470451eaed1ca';
+
 $userId = 'U4eec00d7cad2ba254335e7a82082aba2';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-$headers = array('Authorization: Bearer ' . $access_token);
+// $headers = array('Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// $result = curl_exec($ch);
+// curl_close($ch);
 
-echo $result['displayName'];
+// echo $result;
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-$response = $bot->getProfile('<userId>');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$response = $bot->getProfile($userId);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
