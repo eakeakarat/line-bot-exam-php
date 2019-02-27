7fd18349e7d4afb3fb3e5ccd3ae375fa8a1ae217
Eakkk <eakarat.sa@u.th>
2019-02-28 00:05:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 					'type' => 'text',
-					'text' => $event['message']['type']
+					'text' => $event['message']['text']
 				];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
