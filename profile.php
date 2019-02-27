dfd244028840d7eb29c6b1be72097735e6bc68c3
Eakkk <eakarat.sa@u.th>
2019-02-28 00:19:34 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,15 +6,13 @@ $userId = 'U4eec00d7cad2ba254335e7a82082aba2';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
-// $headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $access_token);
 
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// $result = curl_exec($ch);
-// curl_close($ch);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
 
-// echo $result;
-
-echo $url['displayName'];
+echo $result['displayName'];
