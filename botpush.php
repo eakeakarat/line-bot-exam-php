f238fe0394db706320109afc0dad48ae16f0af3f
Eakkk <eakarat.sa@u.th>
2019-02-27 22:08:05 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'fJf+0FIVDfQ4fBhxYqleC320uQ5ySjt2cPYYbzeDTShv/PNdX8n47JyGCVuGRAE
 
 $channelSecret = '6c82b4408e6a4b534ed470451eaed1ca';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U4eec00d7cad2ba254335e7a82082aba2';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
