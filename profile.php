d5c7b3fd3a0e0ce3b3bc0ee242e297b009e973b4
Eakkk <eakarat.sa@u.th>
2019-02-28 00:26:53 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -16,3 +16,14 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result['displayName'];
+
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$response = $bot->getProfile('<userId>');
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
