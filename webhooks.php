d9e1c0b3118ff13c1ba4080b7d6efbe09d65c304
Eakkk <eakarat.sa@u.th>
2019-03-01 11:21:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' ){
+		if ($event['type'] == 'follow' ){
 			$text = $event['source']['userId'];
 			$fp = fopen('user.csv','a');
 			fwrite($fp,$text);
